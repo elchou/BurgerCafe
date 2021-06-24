147f62318dfcf946d6177f27999e2efc922093af
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2021-06-24 20:09:30 +0400

    change javascript file, remove spaces

--- main.js
+++ main.js
@@ -4,14 +4,11 @@ let menuOpen = false;
 menuBtn.addEventListener('click', ()=> {
     if(!menuOpen) {
         menuBtn.classList.add('open');
-        navigation.classList.add('show');
-
+        navigation.classList.add('show')
 
         menuOpen=true;
     }
     else {
-
-
         menuBtn.classList.remove('open');
         navigation.classList.remove('show');
 
