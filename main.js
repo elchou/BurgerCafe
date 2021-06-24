3f3a4424f8d6e4cf8669cabced1f3e178f61f4cf
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2021-06-24 20:06:23 +0400

    add resp

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+let menuOpen = false;
+menuBtn.addEventListener('click', ()=> {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        navigation.classList.add('show');
+
+
+        menuOpen=true;
     }
-  }
+    else {
+
+
+        menuBtn.classList.remove('open');
+        navigation.classList.remove('show');
+
+
+        menuOpen=false;
+    }
+})
